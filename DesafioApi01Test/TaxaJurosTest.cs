f3e5e09e0033cddbfd71ea2e5d24e5f655416004
Lucas Pompeu Neves <lucaspompeuneves@gmail.com>
2021-07-14 21:38:42 -0300

    Implementação de testes unitários

---
+++ DesafioApi01Test/TaxaJurosTest.cs
@@ -0,0 +1,20 @@
+using DesafioApi01.Model;
+using DesafioApi01.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesafioApi01Test
+{
+    [TestClass]
+    public class TaxaJurosTest
+    {
+        [TestMethod]
+        public void TaxaJuros()
+        {
+            Taxa expected = new Taxa { ValorTaxa = 0.1 };
+
+            Taxa actual = TaxaService.GetTaxaJuros();
+
+            Assert.AreEqual(expected.ValorTaxa, actual.ValorTaxa);
+        }
+    }
+}
